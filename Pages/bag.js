2ebff566eaa270c8f6589135fd54c15257772247
sunnyshinde123 <sunnyshinde157@gmail.com>
2024-05-25 18:41:50 +0530

    up to date

--- Pages/bag.js
+++ Pages/bag.js
@@ -1,3 +1,4 @@
+const ConvenienceFee=99;
 let displayBagObj;
 onLoad();
 
@@ -5,6 +6,41 @@ onLoad();
 function onLoad(){
     getBagDeatailsFromStore();
     displayBag();
+    displayBagSummary();
+}
+
+function displayBagSummary(){
+    let getSummaryData=document.querySelector('.price-data');
+    let totalMRP=0;
+    let discountMRP=0;
+    let totalAmt=0;
+
+    displayBagObj.forEach((ele)=> {
+        totalMRP+=ele.original_price;
+        discountMRP+=ele.original_price-ele.current_price;
+    })
+
+    totalAmt+=totalMRP-discountMRP+ConvenienceFee;
+    getSummaryData.innerHTML=`<div class="one-grid">
+    <p>PRICE DETAILS (${displayBagObj.length} items)</p>
+</div>
+<div class="two-grid">
+    <div class="two-grid-one">
+        <p>Total MRP</p>
+        <p>Discount MRP</p>
+        <p>Convenience Fee</p>
+        <p><b>Total Amount</b></p>
+    </div>
+    <div class="two-grid-two">
+        <p>&#8377;${totalMRP}</p>
+        <p>&#8377; ${discountMRP}</p>
+        <p>&#8377; ${ConvenienceFee}</p>
+        <p><b>&#8377; ${totalAmt}</b></p>
+    </div>
+</div>
+<div class="third-grid">
+    <button class="btn btn-primary">PLACE ORDER</button>
+</div>`
 }
 
 function getBagDeatailsFromStore(){
@@ -15,7 +51,6 @@ function getBagDeatailsFromStore(){
             }
         }
     })
-    console.log(displayBagObj);
 }
 
 function displayBag(){
@@ -24,14 +59,24 @@ function displayBag(){
     displayBagObj.forEach(element => {
         htmlPart+=generateOneItem(element);
     });
-    console.log(htmlPart);
     getMain_left.innerHTML=htmlPart;
 
 
 }
 
+function removeBagItem(itemId){
+    console.log('click'+itemId);
+    storeBag=storeBag.filter((ele)=> ele !=itemId)
+    console.log(storeBag);
+    localStorage.setItem('storeBag', JSON.stringify(storeBag));
+    getBagDeatailsFromStore();
+    displayIconCount();
+    displayBag();
+    displayBagSummary();
+
+}
+
 function generateOneItem(item){
-    console.log(item);
     return `<div class="main-left">
     <div class="img-cover">
         <img src="../${item.image}" alt="">
@@ -46,7 +91,8 @@ function generateOneItem(item){
         Delivery by <span style="color: greenyellow; font-weight: 900;">${item.delivery_date}</span> </p>
     </div>
     <div>
-        <button class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+        <button class="btn btn-danger" onclick="removeBagItem(${item.id})"><i class="fa-solid fa-trash"></i></button>
     </div>
 </div>`
 }
+
